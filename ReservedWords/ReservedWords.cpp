b6c6bed7e6488160e2430607bb615b4be65a3898
Kaleb Soller <kalebsoller@gmail.com>
2024-02-05 20:51:01 -0500

    Project template added for future tutorial.

---
+++ ReservedWords/ReservedWords.cpp
@@ -0,0 +1,174 @@
+// ReservedWords.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+/* LIST OUT ALL OF THE 92 RESERVED KEYWORDS. 
+ * Create a program to learn how to use each reserved keyword.
+ * 
+ * alignas
+ * alignof
+ * and
+ * and_eq
+ * asm
+ * auto
+ * bitand
+ * bitor
+ * bool
+ * break
+ * case
+ * catch
+ * char
+ * char8_t (c++20)
+ * char16_t
+ * char32_t
+ * class
+ * compl
+ * concept (c++20)
+ * const
+ * consteval (c++20)
+ * contexpr
+ * constinit (c++20)
+ * const_cast
+ * continue
+ * co_await (c++20)
+ * co_return (c++20)
+ * co_yield (c++20)
+ * decltype
+ * default
+ * delete
+ * do
+ * double
+ * dynamic_cast
+ * else
+ * enum
+ * explicit
+ * export
+ * extern
+ * false
+ * float
+ * for
+ * friend
+ * goto
+ * if
+ * inline
+ * int
+ * long
+ * mutable
+ * namespace
+ * new
+ * noexcept
+ * not
+ * not_eq
+ * nullptr
+ * operator
+ * or
+ * or_eq
+ * private
+ * protected
+ * public
+ * register
+ * reinterpret_cast
+ * requires (c++20)
+ * return
+ * short
+ * signed
+ * sizeof
+ * static
+ * static_assert
+ * static_cast
+ * struct
+ * switch
+ * template
+ * this
+ * thread_local
+ * throw
+ * true
+ * try
+ * typedef
+ * typeid
+ * typename
+ * union
+ * unsigned
+ * using
+ * virtual
+ * void
+ * volatile
+ * wchar_t
+ * while
+ * xor
+ * xor_eq
+ * 
+ *
+ *
+ * 
+ *
+ *
+ * 
+ *
+ *
+ * 
+ *
+ *
+ * 
+ *
+ *
+ * 
+ *
+ *
+ * 
+ *
+ *
+ * 
+ *
+ *
+ * 
+ *
+ *
+ * 
+ *
+ *
+ * 
+ *
+ *
+ * 
+ *
+ *
+ * 
+ *
+ *
+ * 
+ *
+ *
+ * 
+ *
+ *
+ * 
+ *
+ *
+ * 
+ *
+ *
+ * 
+ *
+ *
+ * 
+ *
+ *
+ * 
+ *
+ *
+ * 
+ *
+ *
+ *
+ *
+ */
