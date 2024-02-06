0367028459c546358a91560d36d2f08fc3cb7068
Kaleb Soller <kalebsoller@gmail.com>
2024-02-06 18:31:15 -0500

    forgot to commit changs yesterday. ch2.2 updates

--- Ch2/main.cpp
+++ Ch2/main.cpp
@@ -1,8 +1,32 @@
 // Main project file to work with my other files.
 
+
+
 #include <iostream>
 
 int main()
 {
 	return 0;
+}
+
+
+//TODO: Fix these from tutorial
+//TODO: TASK 2
+TypeNamePlaceholder()
+{
+
+}
+
+try
+{
+
+}
+catch (const std::exception&)
+{
+
+}
+
+for (size_t i = 0; i < length; i++)
+{
+
 }
