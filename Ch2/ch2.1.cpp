be4dd30bee42df68ee35dfdf76bed4329da6c15a
Kaleb Soller <kalebsoller@gmail.com>
2024-02-05 21:29:33 -0500

    Chapter 2.1 complete. Moving on.

---
+++ Ch2/ch2.1.cpp
@@ -0,0 +1,32 @@
+// file structure from memory
+
+#include <iostream>
+
+void doPrint()
+{
+	std::cout << "Do Print\n";
+}
+
+void doB()
+{
+	std::cout << "In doB()\n";
+}
+
+void doA()
+{
+	std::cout << "Starting doA()\n";
+	doB();
+	std::cout << "Ending doA()\n";
+}
+
+void main() // I guess this main function does not have to have an 'int' return.
+{
+	//data
+	std::cout << "Starting doPrint():\n";
+	doPrint();	//	Interrupt main() by making a function call to doPrint().
+	doA();		//	Start nested functions by calling doA().
+	doPrint();	//	Call doPrint a second time.
+	std::cout << "Ending main(). \n";
+
+	//return 0;
+}
---
+++ Ch2/ch2.2.cpp
@@ -0,0 +1,15 @@
+// CHAPTER 2.2 
+// Lesson based on the web:
+// https://www.learncpp.com/cpp-tutorial/function-return-values-value-returning-functions/
+
+#include <iostream>
+
+
+void main()
+{
+	std::cout << "Enter an integer";
+	int num{};
+	std::cin >> num;
+
+
+}
