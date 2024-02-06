b5cc8aa726308183b1b0215075070784f9042a68
Kaleb Soller <kalebsoller@gmail.com>
2024-02-05 19:40:01 -0500

    Create new project. Ch. 2.

---
+++ Ch1 Project.cpp
@@ -0,0 +1,14 @@
+// Delete everything. All code was written by hand.
+
+#include <iostream>;
+
+int main()
+{
+	std::cout 
+		<< "Enter and Integer:";
+
+	int inVariable{};
+	std::cin
+		>> inVariable;
+	return 0;
+}
