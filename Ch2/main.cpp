a4f26b519c11c4c677f7d4b0e299a571f2969b95
Kaleb Soller <kalebsoller@gmail.com>
2024-02-06 19:53:36 -0500

    Chapter complete. Ch2.4

--- Ch2/main.cpp
+++ Ch2/main.cpp
@@ -1,7 +1,7 @@
 // Main project file to work with my other files.
 // Learning tutorial based on website: 
-// https://www.learncpp.com/cpp-tutorial/void-functions-non-value-returning-functions/
-// CH 2.3 Tutorial 'VOID FUNCTIONS' (non-value returning functions.)
+// https://www.learncpp.com/cpp-tutorial/introduction-to-function-parameters-and-arguments/
+// CH 2.4 Introduction to function parameters and arguments
 
 /* PRINCIPLES USED
 * 
@@ -15,11 +15,17 @@
 * 
 */
 
-#include <iostream>	//	WHY: Used for console I/O
+#include <iostream>//	WHY: Used for console I/
 #include <cstdlib>	//	WHY: Used for main() returns, EXIT_SUCCESS.
-//	CTSDLIB = Constant Standard Library. Yes, I believe this imports everything.
-//	Although, I'm not sure if this is needed. It seems to be working without this header refernce.
-//	TODO: Is <cstdlib.h> needed? Look into it to understand why it is importing automatically.
+
+int getValueFromUser2()
+{
+	std::cout << "Enter and Integer: ";
+	int input{};
+	std::cin >> input;
+
+	return input;
+}
 
 
 void returnNothing()
@@ -34,21 +40,49 @@ void printHi()
 	//	No return needed. Function is type VOID.
 }
 
+//	WHAT: Used to demonstrate a function parameter.
+void printValue(int x, int y)
+{
+	std::cout << "Print x: " << x << '\n';
+	std::cout << "Print y: " << y << '\n';
+}
+
 int returnFive()
 {
 	//	return 5& '\n';	//	NOTE: Runtime Error. Cannot return a new line of int.
 	return 5;
 }
 
+//	WHAT: DRY principles. Moved to function to not duplicate code.
+void printDouble(int num)
+{
+	
+	std::cout << num << " doubled is: " << num * 2 << '\n';
+}
+
+int add(int x, int y)
+{
+	return x + y;
+}
+
+void doPrint2()
+{
+	std::cout << "In doPrint()" << '\n';
+}
+
+int multiply(int a, int b)
+{
+	return a * b;
+}
+
 int main()
 {
-	printHi();	//	WHY: Calls for proof of conepts for VOID functions.
-	//	std::cout << printHi();	//	WHY: Used to demonstrate a compile error.
-	
-	returnNothing();
-	returnFive();
-
-	std::cout << returnFive() << '\n';;
+	//	int num{ getValueFromUser2() };
+	//	printDouble(num);
+	printDouble(getValueFromUser2());	//	Combine the above two lines of code.
+	doPrint2();
+	printValue(6,1);
+	std::cout << "Add 2 + 3 = " << add(2, 3) << '\n';
 		
 	return EXIT_SUCCESS;
 }
